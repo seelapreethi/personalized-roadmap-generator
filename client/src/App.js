71255dd2496aefc0d938c3b237eb8eca857a49e7
Seela Preethi <preethiseela8@gmail.com>
2025-07-30 19:51:15 +0530

    Configure routes in App.js for login, dashboard, and register

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import RegisterPage from './pages/RegisterPage';
+import DashboardPage from './pages/DashboardPage';
 
 
 function App() {
@@ -10,6 +11,8 @@ function App() {
       <Routes>
         <Route path="/login" element={<LoginPage />} />
         <Route path="/register" element={<RegisterPage />} />
+        <Route path="/dashboard" element={<DashboardPage />} />
+
 
         {/* Add more routes here like Register, Dashboard later */}
       </Routes>
