97bb424ece5b6fd3be89ddcd3577d1dc6993e20a
Seela Preethi <preethiseela8@gmail.com>
2025-08-01 20:10:00 +0530

    Updated App.js

--- client/src/App.js
+++ client/src/App.js
@@ -4,7 +4,7 @@ import LoginPage from "./pages/LoginPage";
 import RegisterPage from './pages/RegisterPage';
 import DashboardPage from './pages/DashboardPage';
 import LearningFormPage from './pages/LearningFormPage';
-
+import FormPage from './pages/FormPage';
 
 function App() {
   return (
@@ -14,6 +14,8 @@ function App() {
         <Route path="/register" element={<RegisterPage />} />
         <Route path="/dashboard" element={<DashboardPage />} />
 <Route path="/learning-form" element={<LearningFormPage />} />
+<Route path="/form" element={<FormPage />} />
+
 
 
         {/* Add more routes here like Register, Dashboard later */}
