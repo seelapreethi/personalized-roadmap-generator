442d91bbf7bbd9fe4ccd4adfe508308ed8a419dd
Seela Preethi <preethiseela8@gmail.com>
2025-08-01 20:14:39 +0530

    Add backend route to handle learning form submissions

--- server/routes/learningFormRoutes.js
+++ server/routes/learningFormRoutes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const { submitLearningForm } = require('../controllers/learningFormController');
-const authMiddleware = require('../middleware/auth');
+const authMiddleware = require('../middleware/authMiddleware.js');
 
 router.post('/', authMiddleware, submitLearningForm);
 
