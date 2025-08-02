160368cee12d0ea670283ec2a6b81d4c33eed929
Seela Preethi <preethiseela8@gmail.com>
2025-08-02 17:03:51 +0530

    Updated LearningForm.js

--- server/models/LearningForm.js
+++ server/models/LearningForm.js
@@ -1,30 +1,19 @@
 const mongoose = require('mongoose');
 
-const LearningFormSchema = new mongoose.Schema({
+const formSchema = new mongoose.Schema({
   user: {
     type: mongoose.Schema.Types.ObjectId,
     ref: 'User',
     required: true,
   },
-  topics: {
-    type: [String],
-    required: true,
+  goal: String,
+  skillLevel: String,
+  weeklyTime: String,
+  topics: String,
+  createdAt: {
+    type: Date,
+    default: Date.now,
   },
-  skillLevel: {
-    type: String,
-    enum: ['Beginner', 'Intermediate', 'Advanced'],
-    required: true,
-  },
-  weeklyTime: {
-    type: Number,
-    required: true,
-  },
-  goal: {
-    type: String,
-    required: true,
-  },
-}, {
-  timestamps: true,
 });
 
-module.exports = mongoose.model('LearningForm', LearningFormSchema);
+module.exports = mongoose.model('LearningForm', formSchema);
