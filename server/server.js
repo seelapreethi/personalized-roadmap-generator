a01039a0d3ab165d078baa8be7c86d26cacc7495
Seela Preethi <preethiseela8@gmail.com>
2025-08-12 20:32:53 +0530

    Updated server.js

--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const dotenv = require('dotenv');
 const connectDB = require('./config/db');
 const cors = require('cors');
+const resourcesRoutes = require("./routes/resources");
 
 const app = express();
 dotenv.config();
@@ -17,6 +18,7 @@ app.use('/api/learning-form', learningFormRoutes);
 const formRoutes = require('./routes/formRoutes');
 app.use('/api/learning-form', require('./routes/formRoutes'));
 app.use('/api/roadmap', require('./routes/roadmapRoutes'));
+app.use("/api/resources", resourcesRoutes);
 
 
 
